da19b20fd58dc1023e87b601861ce21083837ab8
tmessall <messalltt@s.dcsdk12.org>
2018-12-07 08:28:06 -0700

    Complete exercise 3

--- src/PictureTester.java
+++ src/PictureTester.java
@@ -16,6 +16,14 @@ public class PictureTester
     beach.zeroBlue();
     beach.explore();
   }
+
+  public static void testKeepOnlyBlue()
+  {
+    Picture beach = new Picture("beach.jpg");
+    beach.explore();
+    beach.keepOnlyBlue();
+    beach.explore();
+  }
   
   /** Method to test mirrorVertical */
   public static void testMirrorVertical()
@@ -59,7 +67,7 @@ public class PictureTester
     // and comment out the ones you don't want
     // to run
     testZeroBlue();
-    // testKeepOnlyBlue();
+    testKeepOnlyBlue();
     // testKeepOnlyRed();
     // testKeepOnlyGreen();
     // testNegate();
